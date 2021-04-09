5f64394beda4d3a0db601f78f3425aa3fc472124
user <user@DESKTOP-E6VOGN1>
2021-04-09 13:34:09 +0530

    Updated Hibernate Project2...

---
+++ Hibernateproj2/src/com/jspiders/hibernate/HibernateTest.java
@@ -0,0 +1,53 @@
+package com.jspiders.hibernate;
+
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.jspiders.dto.Address;
+import com.jspiders.dto.userDetails128;
+
+public class HibernateTest {
+
+	public static void main(String[] args) {
+		
+		userDetails128 user= new userDetails128();
+		user.setUserName("vishal");
+		user.setJoinedDate(new Date());
+		user.setUserId(1234);
+		
+		Address a1 = new Address();
+		a1.setCity("Banglore");
+		a1.setState("karnataka");
+		a1.setPincode(560004);
+		a1.setCountry("india");
+		user.setHomeAddress(a1);
+		
+		Address a2 = new Address();
+		a2.setCity("Bidar");
+		a2.setState("karnataka");
+		a2.setPincode(585401);
+		a2.setCountry("uganda");
+		user.setWorkAddress(a2);
+		
+		userDetails128 user1= new userDetails128();
+		user1.setUserName("Anand");
+		user1.setJoinedDate(new Date());
+		user1.setUserId(1234);
+		
+		
+		
+	SessionFactory sessionFactory=	new Configuration().configure().buildSessionFactory();
+	Session session=sessionFactory.openSession();
+	session.beginTransaction();
+
+	session.save(user);
+	session.getTransaction().commit();	
+	session.close();
+	
+}
+}
